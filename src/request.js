e5356daefaf6c4b60c377fea44b36e1089b4f875
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-05-09 11:34:14 +0000

    ?

--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 
 async function requestData(data) {
 
-    await fetch(`http://api.weatherapi.com/v1/current.json?key=ac11e311dc59479c849114218230505&q=${data}`)
+    await fetch(`https://api.weatherapi.com/v1/current.json?key=ac11e311dc59479c849114218230505&q=${data}`)
     .then(async function(response) {
         
         return await response.json();
@@ -9,6 +9,9 @@ async function requestData(data) {
     .then(function(response) {
         const temp = document.querySelector(".temperature-display");
         temp.innerHTML =  `${response.current.temp_c}`;
+    })
+    .catch(function(error) {
+        console.log(error);
     })
 
 }
