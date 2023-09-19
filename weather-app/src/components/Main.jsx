5052bd6f1b83a2ee1a5f9a96567d412926d64065
stpafk <stephan.weigert@hotmail.com>
2023-09-19 20:44:13 -0300

    start main sect changes

--- weather-app/src/components/Main.jsx
+++ weather-app/src/components/Main.jsx
@@ -4,6 +4,15 @@ import Handler from "./Handler";
 function Main() {
 
     const [city, setCity] = useState("Medicine Hat");
+    const [obj, setObj] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/current.json?key=ac11e311dc59479c849114218230505&q=${city}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setObj({...data})})
+    }, [city])
 
     function changeState() {
         let city = document.querySelector(".container.input").value;
@@ -18,7 +27,7 @@ function Main() {
                         <input className="container input" type="text" placeholder="Example: New York"/>
                         <button onClick={changeState}>Search</button>
                     </div>
-                    <Handler city={city}/>
+                    <Handler obj={obj}/>
               </section>
           </main>
     )
