a85676eccb95f6ff4c11f3297e43003c2ab5086f
stpafk <stephan.weigert@hotmail.com>
2023-09-18 22:10:06 -0300

    add request

--- weather-app/src/Handler.jsx
+++ weather-app/src/Handler.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 
 
-function Handler() {
+function Handler(props) {
 
+    const [temp, setTemp] = useState(0);
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/current.json?key=ac11e311dc59479c849114218230505&q=${props.city}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setTemp(data.current.temp_c)
+        })
+    })
     
+    return(
+        <h1>{temp}</h1>
+    )
 }
 
 export default Handler;
