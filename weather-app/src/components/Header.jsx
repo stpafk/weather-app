d1da8da84570274448f10e3f82c64a408534be30
stpafk <stephan.weigert@hotmail.com>
2023-09-19 09:49:29 -0300

    add icons

--- weather-app/src/components/Header.jsx
+++ weather-app/src/components/Header.jsx
@@ -2,8 +2,8 @@
 function Header() {
     return(
         <header>
-              <img src="https://img.icons8.com/?size=58&id=lnlY2wBk8BW5&format=png" alt="cloud image" />
-              <h1>Weather App</h1>
+                <i class="bi bi-cloud-sun"></i>
+                <h1>Weather Peek</h1>
           </header>
     )
 }
