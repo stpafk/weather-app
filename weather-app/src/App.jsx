f031b5ff48130df899a1acbddb037ca8ca492c5b
stpafk <stephan.weigert@hotmail.com>
2023-09-18 22:18:00 -0300

    start temp changes

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function App() {
 
-    const [city, setCity] = useState("Curitiba");
+    const [city, setCity] = useState("Medicine Hat");
 
     function changeState() {
         let city = document.querySelector(".container.input").value;
