528cba261350214f8c024934bff3d632b1d97a07
stpafk <stephan.weigert@hotmail.com>
2023-09-19 09:10:10 -0300

    add components

---
+++ weather-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+    
+    return(
+        <footer>
+              <div className="wrapper">
+                  <p>Created By</p>
+                  <a href="https://github.com/stpafk">stpafk</a>
+                  <img src="https://avatars.githubusercontent.com/u/117909784?s=40&v=4" alt="stephan photo" />
+              </div>
+          </footer>
+    )
+
+}
+
+export default Footer;
