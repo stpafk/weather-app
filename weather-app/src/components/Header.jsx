ea7fab747d8c7c09d92a101beacffcd31fd414d3
stpafk <stephan.weigert@hotmail.com>
2023-09-19 10:34:10 -0300

    add style to header

--- weather-app/src/components/Header.jsx
+++ weather-app/src/components/Header.jsx
@@ -1,10 +1,15 @@
+import rain from "../assets/rain.gif"
+
 
 function Header() {
     return(
         <header>
-                <i class="bi bi-cloud-sun"></i>
-                <h1>Weather Peek</h1>
-          </header>
+                <img src={rain} alt="rain gif"/>
+                <div className="header__main">
+                    <i class="bi bi-cloud-sun"></i>
+                    <h1>Weather Peek</h1>
+                </div>
+        </header>
     )
 }
 
