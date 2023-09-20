92092d8246f0eadfbe0b89386e77cec90d4aeffa
stpafk <stephan.weigert@hotmail.com>
2023-09-20 08:13:11 -0300

    fixed use effect

--- weather-app/src/components/Handler.jsx
+++ weather-app/src/components/Handler.jsx
@@ -3,6 +3,16 @@ import { useEffect, useState } from "react";
 function Handler(props) {
 
     const obj = props.obj;
+    const [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${obj === null ? "Medicine Hat" : obj.city}&units=metric&appid=1b03a67987404c2ea16169c823e9c21d`)
+            .then((response) => response.json())
+            .then((data) => {
+                setForecast(data);
+                console.log(data)
+            })
+    }, [])
 
     if (props.obj === null) {
         return <h1>Fetching...</h1>
@@ -13,6 +23,8 @@ function Handler(props) {
             <h1>{obj.location.name}</h1>
             <h3>{obj.location.region}, {obj.location.country}</h3>
             <h1>{obj.current.temp_c}°C</h1>
+
+            <h1></h1>
         </div>
     )
 }
--- weather-app/src/components/Header.jsx
+++ weather-app/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <header>
                 <img src={rain} alt="rain gif"/>
                 <div className="header__main">
-                    <i class="bi bi-cloud-sun"></i>
+                    <i className="bi bi-cloud-sun"></i>
                     <h1>Weather Peek</h1>
                 </div>
         </header>
