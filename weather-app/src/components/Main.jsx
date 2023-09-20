3fafd25a867e0ff9e532d668878c92fe7a33fc1b
stpafk <stephan.weigert@hotmail.com>
2023-09-20 08:39:02 -0300

    moved effect to parent

--- weather-app/src/components/Main.jsx
+++ weather-app/src/components/Main.jsx
@@ -5,12 +5,20 @@ function Main() {
 
     const [city, setCity] = useState("Medicine Hat");
     const [obj, setObj] = useState(null)
+    const [forecast, setForecast] = useState(null)
 
     useEffect(() => {
         fetch(`https://api.weatherapi.com/v1/current.json?key=ac11e311dc59479c849114218230505&q=${city}`)
         .then((response) => response.json())
         .then((data) => {
-            setObj({...data})})
+            setObj({...data})
+            return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric%cnt=3&appid=1b03a67987404c2ea16169c823e9c21d`)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setForecast(data);
+            console.log(data)
+        })
     }, [city])
 
     function changeState() {
@@ -26,7 +34,7 @@ function Main() {
                         <input className="container input" type="text" placeholder="Example: New York"/>
                         <button onClick={changeState}>Search</button>
                     </div>
-                    <Handler obj={obj}/>
+                    <Handler obj={obj} forecast={forecast}/>
               </section>
           </main>
     )
