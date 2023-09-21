d649969392140ef1a047621e0ca643090b2e4463
stpafk <stephan.weigert@hotmail.com>
2023-09-21 09:51:25 -0300

    feat: cool clouds

--- weather-app/src/components/Footer.jsx
+++ weather-app/src/components/Footer.jsx
@@ -8,6 +8,9 @@ function Footer() {
                   <a href="https://github.com/stpafk">stpafk</a>
                   <img src="https://avatars.githubusercontent.com/u/117909784?s=40&v=4" alt="stephan photo" />
               </div>
+              <div className="clouds" id="0"></div>
+              <div className="clouds" id="1"></div>
+              <div className="clouds" id="2"></div>
           </footer>
     )
 
