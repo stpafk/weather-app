2aca98e3345983134b232a3550767fc8439778c1
stpafk <stephan.weigert@hotmail.com>
2023-09-21 07:54:17 -0300

    style: correct img size

--- weather-app/src/components/Handler.jsx
+++ weather-app/src/components/Handler.jsx
@@ -19,27 +19,26 @@ function Handler(props) {
                     <i class="bi bi-thermometer-snow"></i>}</span>
                     <span className="text">{obj.current.temp_c}°C</span>
                 </div>
+                <h3>{obj.current.condition.text}</h3>
                 <h1>{obj.location.name}</h1>
                 <h3>{obj.location.region}, {obj.location.country}</h3>
                 <h3 className="general tz">{obj.location.tz_id}</h3>
                 <h3 className="general lu">Last Updated {obj.current.last_updated}</h3>
             </div>
             <ul className="weather extra">
-                <li className="extra__li condition">
-                    <span className="icon"><img src={obj.current.condition.icon} 
-                    alt={obj.current.condition.text} /></span>
-                    <span className="text">{obj.current.condition.text}</span>
-                </li>
-                <li className="extra__li">Feels Like
+                <label htmlFor="Feels Like">Feels Like</label>
+                <li className="extra__li">
                     <span className="icon"><img src={weather}
                     alt="person standing next to termomether" /></span>
                     <span className="txt">{obj.current.feelslike_c}°C</span>
                 </li>
-                <li className="extra__li">Humidity
+                <label htmlFor="Humidity">Humidity</label>
+                <li className="extra__li">
                     <span className="icon"><img src={humidity} alt="humidity icon" /></span>
                     <span className="txt">{obj.current.humidity}%</span>
                 </li>
-                <li className="extra__li">Wind
+                <label htmlFor="Wind">Wind</label>
+                <li className="extra__li">
                     <span className="icon"><i class="bi bi-wind"></i></span>
                     <span className="txt">{obj.current.wind_kph}K/PH</span>
                 </li>
